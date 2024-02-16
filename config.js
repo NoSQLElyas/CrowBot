17f93566edea219a1f8cd2df5a44fbd52e8aece9
NoSQLElyas <154375156+NoSQLElyas@users.noreply.github.com>
2024-02-16 16:50:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  token: "MTEwOTAzMjQ0OTk3MjkxNjIyNQ.GZClgg.3mtSi9U5KX5W3nRatMRzO-YSgS6vZHg0vXk6cM", //Le token de votre bot
+  "buyers": [
+    "1086975010717061171" //Votre id
+  ],
+  "prefix": "+", //Laisser ce prefix tel quel si vous voulez que le système de modmail (ticket) marchent !!
+
+  "panel": "", //Ne rien mettre !!!
+
+  "default_color": "#5a65ff", //Couleur des embeds. Possible :  rouge: #FF0000,vert: #00FF00,bleu: #0000FF,noir: #000000,blanc: #FFFFFF,rose: #dc14eb,violet: #764686,orange: #FFA500,jaune: #FFFF00,marron: #A52A2A,gris: #808080, argent: #C0C0C0,cyan: #00FFFF,lavande: #E6E6FA,corail: #FF7F50,beige: #F5F5DC,
+}; 
